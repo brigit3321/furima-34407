8e636266c4834e070eabbe0dc96feaa08ec3e265
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-03-10 16:28:03 +0900

    単体テストの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     nickname { 'test太郎' }
     email {Faker::Internet.free_email}
     password {  Faker::Internet.password(min_length: 6) }
-    #password_confirmation { password }
     last_name {"山田"}
     first_name {"太郎"}
     last_name_kana {"ヤマダ"}
