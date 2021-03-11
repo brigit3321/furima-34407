60d0f0fd4460fe082768ae848666e63e51b1c424
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-03-11 16:22:39 +0900

    new,createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
     def index
     end
 
@@ -7,5 +8,11 @@ class ItemsController < ApplicationController
     end
 
     def create
+      @item =  Item.new(item_params)
+    end
+
+    private
+    def item_params
+      params.require(:item).permit(:name, :info, :concept, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :schedule_delivery_id, :price, :image).merge(user_id: current_user.id)
     end
 end
