8c52d09d00a335556107aab9c514ecd1d9a33e85
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-03-12 16:06:12 +0900

    単体テスト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
-    validates :category_id
-    validates :sales_status_id
-    validates :shipping_fee_status_id
-    validates :prefecture_id
-    validates :schedule_delivery_id
     validates :price
   end
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -74,7 +74,19 @@ RSpec.describe Item, type: :model do
       end
 
       it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
-        @item.price = '100'
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+
+      it '販売価格は、¥299以下の場合は保存できないこと' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+
+      it '販売価格は、¥10,000,000以上の場合は保存できないこと' do
+        @item.price = 15000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
@@ -84,6 +96,55 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Half-width number')
       end
+
+      it '販売価格は全角文字では登録できないこと' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number')
+      end
+
+      it '販売価格は半角英数混合では登録できないこと' do
+        @item.price = '1000tttt'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number')
+      end
+
+      it '販売価格は半角英語だけでは登録できないこと' do
+        @item.price = 'tests'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number')
+      end
+
+      it 'category_idが1の場合はでは登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+
+      it 'sales_status_idが1の場合はでは登録できないこと' do
+        @item.sales_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Sales status Select')
+      end
+
+      it 'shipping_fee_status_idが1の場合はでは登録できないこと' do
+        @item.shipping_fee_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping fee status Select')
+      end
+
+      it 'prefecture_idが1の場合はでは登録できないこと' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture Select')
+      end
+
+      it 'schedule_delivery_idが1の場合では登録できないこと' do
+        @item.schedule_delivery_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Schedule delivery Select')
+      end
+
     end
   end
 end
