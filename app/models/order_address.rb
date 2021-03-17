4a6b4955f4a5402968bfeed50399c58370924d8b
brigit3321 <qqq44cm9@yahoo.co.jp>
2021-03-17 14:13:45 +0900

    商品購入機能の実装完了

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,8 +1,9 @@
 class OrderAddress
     include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :order_id
+    attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :order_id, :token
 
     with_options presence: true do
+        validates :token
         validates :user_id
         validates :item_id
         validates :city
@@ -20,10 +21,8 @@ class OrderAddress
     end
 
     def save
-
         order = Order.create(user_id: user_id, item_id: item_id)
-    
-        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+        Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
     end
 
 
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -78,6 +78,12 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone number Half-width number')
       end
+
+      it "tokenが空では登録できないこと" do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
       
       
     end
